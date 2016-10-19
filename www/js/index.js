90df363b046c0c058ced2337d871318ffacd1fdb
lucatarik <lucatarik@gmail.com>
2016-10-19 16:38:35 +0200

    done gui adding forms

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,57 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener('deviceready', onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
 
-        console.log('Received Event: ' + id);
+}
+
+function openDialog(elem, callback) {
+   if (!(elem instanceof jQuery) )
+   {
+      elem = $(elem);
+   }
+
+   popupDialogObj = $('<div data-role="popup" id="pouppo'+Math.floor(Math.random(100)*10000)+'" data-confirmed="no" data-transition="pop" data-overlay-theme="a" data-theme="a" data-dismissible="true">\
+                           <div data-role="header" data-theme="a"><h1></h1></div>\
+                           <div role="main" class="ui-content"></div>')
+                        .appendTo($.mobile.pageContainer);
+   popupDialogObj.find('.ui-content').append(elem.html());
+   popupDialogObj.trigger('create');
+   popupDialogObj.popup({
+      positionTo:"window",
+      afterclose: function(event, ui) {
+         popupDialogObj.find(".optionConfirm").first().off('click');
+         var isConfirmed = popupDialogObj.attr('data-confirmed') === 'yes' ? true : false;
+         $(event.target).remove();
+         if (isConfirmed && callback) {
+            callback();
+         }
+      },
+              beforeposition: function( event, ui ) {console.log(this,event,ui)},
+              create: function( event, ui ) {popupDialogObj.trigger('reposition');}
+   });
+   popupDialogObj.popup('open');
+   popupDialogObj.find(".optionConfirm").first().on('click', function() {
+      popupDialogObj.attr('data-confirmed', 'yes');
+   });
+}
+
+
+var tplEngine = function(tpl, data) {
+    tpl = tpl.trim();
+    var re = /{{([^}}]+)?}}/g,
+        code = 'var r=[];\n',
+        cursor = 0, match;
+    var add = function(line, js) {
+    js? code += 'r.push("' + data[line] + '");\n' :
+        code += 'r.push("' + line.replace(/"/g, '\\"') + '");\n';
     }
-};
-
-app.initialize();
+    var match;
+    while(match = re.exec(tpl)) {
+        add(tpl.slice(cursor, match.index));
+        add(match[1], true); // <-- say that this is actually valid js
+        cursor = match.index + match[0].length;
+    }
+    add(tpl.substr(cursor, tpl.length - cursor));
+    code += 'return r.join("");'; // <-- return the result
+    return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
+}
